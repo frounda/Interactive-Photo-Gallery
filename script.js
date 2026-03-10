6cde81ed003bdbae2552a416ecc8c29f567cbf7b
frounda <frounda@mcm.edu.ph>
2026-03-10 16:53:43 +0800

    Refactor upDate and unDo functions with comments
    Updated functions to include comments for clarity and changed the order of operations for setting innerHTML and backgroundImage.

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
+/*Name this external file gallery.js*/
 function upDate(previewPic) {
-    console.log("Event triggered! Mouse over: " + previewPic.alt);
-    
-    var displayDiv = document.getElementById('image');
-    
-    // Change background and text
-    displayDiv.style.backgroundImage = "url('" + previewPic.src + "')";
-    displayDiv.innerHTML = previewPic.alt;
+    /* In this function you should 
+       1) change the url for the background image of the div with the id = "image" 
+       to the source file of the preview image
+       2) Change the text  of the div with the id = "image" 
+       to the alt text of the preview image 
+       */
+    document.getElementById('image').innerHTML = previewPic.alt;
+    document.getElementById('image').style.backgroundImage = "url(" + previewPic.src + ")";
 }
 
 function unDo() {
-    var displayDiv = document.getElementById('image');
+    /* In this function you should 
+    1) Update the url for the background image of the div with the id = "image" 
+    back to the orginal-image.  You can use the css code to see what that original URL was
     
-    // Reset background and text
-    displayDiv.style.backgroundImage = "url('')";
-    displayDiv.innerHTML = "Hover over an image below to display here.";
+    2) Change the text  of the div with the id = "image" 
+    back to the original text.  You can use the html code to see what that original text was
+    */
+    document.getElementById('image').style.backgroundImage = "url('')"
+    document.getElementById('image').innerHTML = 'Hover over an image below to display here.';
 }
